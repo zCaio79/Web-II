5029e2be5ab54a39319800e4b48068b2ee9cba8a
zCaio79 <caiomendesfaria@gmail.com>
2024-08-26 18:48:40 -0300

    updated

--- ApiFollowing/assets/script.js
+++ ApiFollowing/assets/script.js
@@ -1,5 +1,8 @@
 
+let searchclick = document.querySelector('.search_button') ;
 
+
+searchclick.addEventListener('click', getFollowingUsers);
 
 
 function getFollowingUsers(){
@@ -7,16 +10,30 @@ function getFollowingUsers(){
     let username = document.querySelector('#user_search').value;
     let friends_container = document.querySelector('.friends_container');
 
+    friends_container.innerHTML = ``;
+
+
     fetch(`https://api.github.com/users/${username}/following`)
     .then(async resp => {
 
         if(!resp.ok){
-            return;
+            throw new Error(resp.status);
         }
 
-        let data = await res.json();
+        let data = await resp.json();
 
         data.map(following => {
+
+            let friend_card = document.createElement('div');
+            friend_card.innerHTML = 
+            `
+            <div class="friend">
+                <img src="${following.avatar_url}">
+                <h4>${following.login}</h4>
+                <a href="${following.html_url}" target="_blank">View Profile</a>
+            </div>
+            `;
+            friends_container.appendChild(friend_card);
 
         })
     })
